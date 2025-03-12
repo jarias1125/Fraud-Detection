3f427a651f8dba23dd01bfa63f55f468a4695e32
Joaquin Arias <joaquin.arias@edu.em-lyon.com>
2025-03-12 01:55:57 +0100

    Add files via upload

---
+++ r_language_placeholder.R
@@ -0,0 +1,11 @@
+# r_language_placeholder.R
+# This is a placeholder file to help GitHub detect R as the primary language.
+
+# Simple R function
+add_numbers <- function(a, b) {
+  return(a + b)
+}
+
+# Example usage
+result <- add_numbers(5, 3)
+print(result)
